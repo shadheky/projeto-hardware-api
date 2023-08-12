15d4254b7c74884d837bc84763f9af99eab1aa54
AllanPetersonS <santospeterson158@gmail.com>
2023-08-12 00:01:52 -0300

    Teste de commit pettlysood

--- Controllers/TesteController.py
+++ Controllers/TesteController.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return 'Hello World!'
 
+
+@app.route('/hello')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+
+
 if __name__ == '__main__':
     app.run()
